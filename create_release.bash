7d765ba92e0cb3db0d8b39c5c526c076f6eb0d53
Steffen Schaumburg <steffen@schaumburger.info>
2015-10-27 12:28:00 +0100

    improve release script

--- create_release.bash
+++ create_release.bash
@@ -3,8 +3,13 @@ echo $1 > src/version.ini
 
 git add src/version.ini
 git commit -m "release $1"
-git tag $1
+git tag -a $1 -m "tag $1"
 
 echo "$1+git" > src/version.ini
 git add src/version.ini
 git commit -m "postrelease for $1"
+
+git push --follow-tags
+
+echo "Update changelog in readme_de.bb.txt"
+echo "Update link+changelog+features in tribe forum and post answer"
